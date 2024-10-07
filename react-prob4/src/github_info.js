73a655c281b7d34221f58bc7ba36fdb07fba6322
KasiditKlang <kasidit.ma@kkumail.com>
2024-10-07 15:52:13 +0700

    react-prob4

--- react-prob4/src/github_info.js
+++ react-prob4/src/github_info.js
@@ -5,13 +5,13 @@ export function GitHubInfo({userInfo}) {
     return(
         <div className="App">
         <img src={userInfo.imgURL} className="image"/>
-        <a href={userInfo.url} target='_blank' rel="noreferrer">{userInfo.alt}({userInfo.followers} followers)</a>
+        <a href={userInfo.url} target='_blank' rel="noreferrer"> {userInfo.alt} ({userInfo.followers} followers)</a>
         </div>
     )} else {
         return(
             <div className="App">
             <img src={userInfo.imgURL} className="image"/>
-            <a href={userInfo.url} target='_blank' rel="noreferrer">{userInfo.alt}</a>
+            <a href={userInfo.url} target='_blank' rel="noreferrer"> {userInfo.alt}</a>
             </div>
         ) 
     } 
